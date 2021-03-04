a4113e1730fdc67d9a70ed0f6662a738ef30dbdd
bugsiesegal <bugsie@segalnyc.com>
2021-03-04 10:35:54 -0500

    Xpath

--- Scraper.py
+++ Scraper.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
+import time
 
 ChromeDriverLocation = os.getcwd() + "/Driver/chromedriver.exe"
 driver = webdriver.Chrome(ChromeDriverLocation)
@@ -12,7 +16,8 @@ ZipCode = "10002"
 
 driver.find_element_by_xpath(ZipCodeInput).send_keys(ZipCode)
 driver.find_element_by_xpath(SearchButton).click()
-VaccinationSites = driver.find_elements_by_class_name("sc-dkQUut TREMO")
+time.sleep(3)
+VaccinationSites = driver.find_elements_by_xpath('//div[@class="sc-dkQUut TREMO"]')
 
 avalibility = []
 
@@ -20,15 +25,16 @@ implimentation = ["walgreens"]
 
 def findsite(VaccinationSites):
     for Site in VaccinationSites:
-        print(Site.find_element_by_class_name("sc-gsWdvU cGnDyB"))
-        if Site.find_element_by_class_name("sc-WZYaI exHCwL").text == "In Stock" and implimentation in Site.find_element_by_class_name("sc-gsWdvU cGnDyB").lower:
+        if Site.find_element_by_xpath('./div[2]/div[2]/div[2]/div/p').text == "In Stock" and implimentation[0] in Site.find_element_by_xpath('./div[2]/div[1]/div').text.lower():
+            print(Site.find_element_by_xpath('./div[2]/div[2]/div[2]/div/p').text)
+            print(Site.find_element_by_xpath('./div[2]/div[1]/div').text)
             avalibility.append(
                 {
-                    "Distance": Site.find_element_by_class_name("sc-bXevSJ doIxg").text,
-                    "Location": Site.find_element_by_class_name("sc-gsWdvU cGnDyB").text
+                    "Distance": Site.find_element_by_xpath('./div[2]/div[2]/div[1]').text,
+                    "Location": Site.find_element_by_xpath('./div[2]/div[1]/div').text
                 }
             )
-            Site.find_element_by_class_name("fas fa-arrow-right").click()
+            # Site.find_element_by_class_name("fas fa-arrow-right").click()
             return None
 
 
@@ -37,3 +43,5 @@ findsite(VaccinationSites)
 # driver.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div/div/div/div/div/div/div[1]/div[1]/a')
 
 print(avalibility)
+
+driver.close()
