3840a5bb3a929695322036c636a587b752d76800
bugsiesegal <bugsie@segalnyc.com>
2021-03-08 10:54:10 -0500

    Getting Git WOrking

--- Scraper.py
+++ Scraper.py
@@ -2,46 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
 import os
 import time
 
+from flask import Flask
+
+options = webdriver.ChromeOptions()
 ChromeDriverLocation = os.getcwd() + "/Driver/chromedriver.exe"
-driver = webdriver.Chrome(ChromeDriverLocation)
-driver.get("https://vaccinefinder.org/search/")
-
-ZipCodeInput = '//*[@id="zipCode"]'
-SearchButton = '//*[@id="split-screen-content"]/form/div[2]/button'
-
-ZipCode = "10002"
-
-driver.find_element_by_xpath(ZipCodeInput).send_keys(ZipCode)
-driver.find_element_by_xpath(SearchButton).click()
-time.sleep(3)
-VaccinationSites = driver.find_elements_by_xpath('//div[@class="sc-dkQUut TREMO"]')
-
-avalibility = []
-
-implimentation = ["walgreens"]
-
-def findsite(VaccinationSites):
-    for Site in VaccinationSites:
-        if Site.find_element_by_xpath('./div[2]/div[2]/div[2]/div/p').text == "In Stock" and implimentation[0] in Site.find_element_by_xpath('./div[2]/div[1]/div').text.lower():
-            print(Site.find_element_by_xpath('./div[2]/div[2]/div[2]/div/p').text)
-            print(Site.find_element_by_xpath('./div[2]/div[1]/div').text)
-            avalibility.append(
-                {
-                    "Distance": Site.find_element_by_xpath('./div[2]/div[2]/div[1]').text,
-                    "Location": Site.find_element_by_xpath('./div[2]/div[1]/div').text
-                }
-            )
-            # Site.find_element_by_class_name("fas fa-arrow-right").click()
-            return None
+options.add_argument("start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--disable-blink-features")
+options.add_argument("--disable-blink-features=AutomationControlled")
+driver = webdriver.Chrome(ChromeDriverLocation, options=options)
+driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Chrome/88.0.4324.192'})
+driver.get("https://www.kroger.com/i/coronavirus-update/vaccine")
 
 
-findsite(VaccinationSites)
-#
-# driver.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div/div/div/div/div/div/div[1]/div[1]/a')
-
-print(avalibility)
-
-driver.close()
